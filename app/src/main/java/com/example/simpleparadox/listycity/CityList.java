af2bdd14b950103ceac3be4d0f8b53ed40113eec
arif047 <94097287+arif047@users.noreply.github.com>
2021-11-29 10:14:27 +0600

    Update CityList.java

--- app/src/main/java/com/example/simpleparadox/listycity/CityList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CityList.java
@@ -1,1 +1,54 @@
+package com.example.simpleparadox.listycity;
+import java.util.ArrayList;
+import java.util.list;
+/**
+*this is a class that keep track of a list of city objects
+*/
+public class CityList{
+  private List<City> cities =new ArrayList();
+  /**
+  *this adds a city to the list if the city doesn't exist
+  *@param city
+  *this is a candidate city to add
+  */
+  public void add(City city){
+    if(cities.contains(city)){
+      throw new IllegalArgumentException();
+    }
+    cities.add(city);
+  }
+   /**
+  *this returns a sorted list of cities
+  *@return
+  *Return the sorted list
+  */ 
+  public List<City> getCities(){
+    List<City> list=cities;
+    Collections.sort(list);
+    return list;
+  }
+
+  /**
+  *this removes a city to the list if the city exist
+  *@remove city
+  *this is a city to remove
+  */
+  public void remove(City city){
+    if(cities.contains(city)){
+       cities.remove(city);
+    }
+    else{
+      throw new IllegalArgumentException();
+    }
+  }
+  /**
+  *this rerturns a city the size of the citylist
+  *@return size of citylist
+  */
+  public void Size(){
+    int siz;
+    siz=cities.size();
+    return siz;
+  }
+}
 
