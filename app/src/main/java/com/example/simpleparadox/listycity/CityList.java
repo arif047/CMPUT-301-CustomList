c9175871b16857613ea6927b5167546b6e68066f
arif047 <94097287+arif047@users.noreply.github.com>
2021-11-29 11:24:06 +0600

    Update CityList.java

--- app/src/main/java/com/example/simpleparadox/listycity/CityList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CityList.java
@@ -1,5 +1,6 @@
 package com.example.simpleparadox.listycity;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.list;
 /**
 *this is a class that keep track of a list of city objects
