1a3b1bcf62a6788cf8f9b13a9fbf945403dee38d
arif047 <94097287+arif047@users.noreply.github.com>
2021-11-29 11:54:12 +0600

    Update CityList.java

--- app/src/main/java/com/example/simpleparadox/listycity/CityList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CityList.java
@@ -1,51 +1,57 @@
 package com.example.simpleparadox.listycity;
+ 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+ 
 /**
-*this is a class that keep track of a list of city objects
-*/
-public class CityList{
-  private List<City> cities =new ArrayList();
-  /**
-  *this adds a city to the list if the city doesn't exist
-  *@param city
-  *this is a candidate city to add
-  */
-  public void add(City city){
-    if(cities.contains(city)){
-      throw new IllegalArgumentException();
+ * This is a class that keeps track of a list of city objects
+ */
+public class CityList {
+    private List<City> cities = new ArrayList<>();
+ 
+    /**
+     * This adds a city to the list if the city does not exist
+     * @param city
+     *      This is a candidate city to add
+     */
+    public void add(City city) {
+        if (cities.contains(city)) {
+            throw new IllegalArgumentException();
+        }
+        cities.add(city);
     }
-    cities.add(city);
-  }
-   /**
-  *this returns a sorted list of cities
-  *@return
-  *Return the sorted list
-  */ 
-  public List<City> getCities(){
-    List<City> list=cities;
-    Collections.sort(list);
-    return list;
-  }
-
-  /**
-  *this removes a city to the list if the city exist
-  *@remove city
-  *this is a city to remove
-  */
-  public void Remove(City city){
-    if(cities.contains(city)){
-       cities.remove(city);
+ 
+    /**
+     * This returns a sorted list of cities
+     * @return
+     *      Return the sorted list
+     */
+    public List<City> getCities() {
+        List<City> list = cities;
+        Collections.sort(list);
+        return list;
     }
-    else{
-      throw new IllegalArgumentException();
+ 
+    /**
+     * This removes a city from the list if the city  exist
+     * @param city
+     *      This is a candidate city to remove
+     */
+  
+   public void Remove(City city) {
+        if (cities.contains(city)) {
+            cities.remove(city);
+        }
+        else{
+            throw new IllegalArgumentException();
+        }
     }
-  }
   /**
-  *this rerturns a city the size of the citylist
-  *@return size of citylist
-  */
+     * This returns the size of the list
+     * @return
+     *      Return the size of the list
+     */
      public int getSizeOfCities(){
         List<City> list = cities;
         return list.size();
